7b25b9ef6a111a95f312defa5913ebfae68d0fa0
Kazumasa Shimomura <kazu.devapp@gmail.com>
2020-04-21 22:15:04 +0900

    Initial Commit

---
+++ Othello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Othello
+//
+//  Created by 下村一将 on 2020/04/21.
+//  Copyright © 2020 Kazumasa Shimomura. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
