aa4a7d5e6e9749c914255c50f5f643998d15975f
Kazumasa Shimomura <kazu.devapp@gmail.com>
2020-04-21 22:36:17 +0900

    Show GameBoard

--- Othello/AppDelegate.swift
+++ Othello/AppDelegate.swift
@@ -1,18 +1,7 @@
-//
-//  AppDelegate.swift
-//  Othello
-//
-//  Created by 下村一将 on 2020/04/21.
-//  Copyright © 2020 Kazumasa Shimomura. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -31,7 +20,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
